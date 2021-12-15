aae5efd77c5c3bb65afd5d24229f0a121304e9b9
Jarcelo <jarcelo@gmail.com>
2021-12-15 18:45:08 -0400

    probando

--- addons/FontAwesome/plugin.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends EditorPlugin
-
-
-func _enter_tree():
-	add_custom_type("FontAwesome", "Label",preload("FontAwesome.gd"), null )
-
-func _exit_tree():
-	remove_custom_type("FontAwesome")
